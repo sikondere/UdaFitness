6891d235da376333f0c615c2b1d28f85f8461139
delta <delta@debian-BULLSEYE-live-builder-AMD64>
2021-10-23 11:49:33 -0400

    initial commit

---
+++ components/AddEntry.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { getMetricMetaInfo } from '../utils/helpers';
+
+export default class AddEntry extends Component {
+
+    state = {
+        run: 0,
+        bike: 0,
+        swim: 0,
+        sleep: 0,
+        eat: 0,
+    };
+
+    increment = (metric) => {
+        const { max, step } = getMetricMetaInfo(metric);
+        this.setState((prevState) => {
+            const count = prevState[metric] + step;
+            return {
+                ...prevState,
+                [metric]: count > max ? max : count,
+            }
+        });
+    };
+
+    decrement = (metric) => {
+        const { step } = getMetricMetaInfo(metric);
+        this.setState((prevState) => {
+            const count = prevState[metric] - step;
+            return {
+                ...prevState,
+                [metric]: count < 0 ? 0 : count,
+            }
+        });
+    };
+
+    slide = (metric, value) => {
+        this.setState(() => ({
+            [metric]: value,
+        }))
+    };
+
+    render() {
+        return (
+            <View>
+                {getMetricMetaInfo('bike').getIcon()}
+            </View>
+        );
+    }
+}
